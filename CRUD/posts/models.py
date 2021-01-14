51fd09dc7eeb15bd279bdcfe4e1ac94dd823b26a
Kim Seohyun <kshjessica@ewhain.net>
2021-01-14 12:53:22 +0900

    post 출력하기까지

--- CRUD/posts/models.py
+++ CRUD/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(verbose_name='제목', max_length=100)
+    writer = models.CharField(verbose_name='글쓴이', max_length=50)
+    content = models.TextField(verbose_name='내용')
+    # DB에는 없는 ORM이라고 함(생성과 업데이트 시각 기록)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # string으로 title 반환
+    def __str__(self):
+        return self.title
--- CRUD/posts/views.py
+++ CRUD/posts/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def post_list(request):
-    return HttpResponse('글 리스트')
+    # return HttpResponse('글 리스트')
+    '''
+    Read(R)
+    포스트들을 불러와서 리스트 형태로 보여준다
+    '''
+    posts = Post.objects.all()
+    ctx = {'posts': posts}  # dictionary
+
+    return render(request, template_name='posts/list.html', context=ctx)
+
+
+def post_detail(request, post_id):
+    '''
+    Read(R)
+    특정 포스트를 불러와서 상세정보를 보여준다
+    '''
+    post = Post.objects.get(id=post_id)
+    ctx = {'post': post}
+
+    return render(request, 'post/detail.html', context=ctx)
